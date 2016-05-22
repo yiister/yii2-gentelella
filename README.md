4d23cccbe1f32c59c6cd20f6814cdfb8db2d90bc
fps01 <fps.06@mail.ru>
2016-05-22 21:37:12 +0300

    init

---
+++ README.md
@@ -0,0 +1,27 @@
+Gentelella admin template for Yii2
+==================================
+
+Free admin template for backend
+
+Installation
+------------
+
+The preferred way to install this extension is through [composer](http://getcomposer.org/download/).
+
+Either run
+
+```
+php composer.phar require --prefer-dist yiister/yii2-gentelella "*"
+```
+
+or add
+
+```
+"yiister/yii2-gentelella": "*"
+```
+
+to the require section of your `composer.json` file.
+
+
+Usage
+-----
---
+++ assets/Asset.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @copyright Copyright (c) 2015 Yiister
+ * @license https://github.com/yiister/yii2-gentelella/blob/master/LICENSE
+ * @link http://gentelella.yiister.ru
+ */
+
+namespace yiister\gentelella\assets;
+
+class Asset extends \yii\web\AssetBundle
+{
+    public $sourcePath = '@vendor/bower/gentelella/production/';
+    public $css = [
+        'css/custom.css',
+    ];
+    public $js = [];
+    public $depends = [
+        'yii\web\YiiAsset',
+        'yii\bootstrap\BootstrapPluginAsset',
+        'rmrevin\yii\fontawesome\AssetBundle',
+    ];
+}
