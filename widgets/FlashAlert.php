aa8450dc6ec6f3da50b7175297aa9024ccaa3189
fps01 <fps.06@mail.ru>
2016-06-11 12:16:59 +0300

    Added a FlashAlert widget (Close #4)

---
+++ widgets/FlashAlert.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * @copyright Copyright (c) 2015 Yiister
+ * @license https://github.com/yiister/yii2-gentelella/blob/master/LICENSE
+ * @link http://gentelella.yiister.ru
+ */
+
+namespace yiister\gentelella\widgets;
+
+use rmrevin\yii\fontawesome\component\Icon;
+use Yii;
+use yii\base\Widget;
+use yii\bootstrap\Alert;
+use yii\helpers\Html;
+
+class FlashAlert extends Widget
+{
+    /**
+     * @var array the HTML attributes for the wrapper tag
+     */
+    public $options = [];
+
+    /**
+     * @var array flashes definition
+     */
+    public $flashes = [
+        'success' => [
+            'class' => 'success',
+            'header' => 'Success!',
+            'icon' => 'check',
+        ],
+        'info' => [
+            'class' => 'info',
+            'header' => 'Info!',
+            'icon' => 'info-circle',
+        ],
+        'warning' => [
+            'class' => 'warning',
+            'header' => 'Warning!',
+            'icon' => 'warning',
+        ],
+        'error' => [
+            'class' => 'error',
+            'header' => 'Error!',
+            'icon' => 'ban',
+        ],
+    ];
+
+    /**
+     * @var bool show an alert header
+     */
+    public $showHeader = false;
+
+    /**
+     * @inheritdoc
+     */
+    public function run()
+    {
+        if (!isset($this->options['id'])) {
+            $this->options['id'] = $this->getId();
+        }
+        echo Html::beginTag('div', $this->options);
+        foreach ($this->flashes as $flashName => $alert) {
+            if (Yii::$app->session->hasFlash($flashName)) {
+                $header = '';
+                if ($this->showHeader) {
+                    $header = Html::tag(
+                        'h4',
+                        (isset($alert['icon']) ? new Icon($alert['icon']) . '&nbsp;' : '') . $alert['header']
+                    );
+                }
+                echo Alert::widget(
+                    [
+                        'body' => $header . Yii::$app->session->getFlash($flashName),
+                        'options' => [
+                            'class' => 'alert alert-' . $alert['class'],
+                        ],
+                    ]
+                );
+            }
+        }
+        echo Html::endTag('div');
+    }
+}
