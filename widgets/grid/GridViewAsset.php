0a7de889a867fc6e00653a3d0a776f48017f679e
MKiselev <maks280795@yandex.ru>
2017-05-22 13:19:02 +0500

    Resolve issue #17

--- widgets/grid/GridViewAsset.php
+++ widgets/grid/GridViewAsset.php
@@ -9,7 +9,7 @@ namespace yiister\gentelella\widgets\grid;
 
 class GridViewAsset extends \yii\web\AssetBundle
 {
-    public $sourcePath = '@vendor/bower/gentelella/vendors/datatables.net-bs/css';
+    public $sourcePath = '@bower/gentelella/vendors/datatables.net-bs/css';
     public $css = [
         'dataTables.bootstrap.min.css',
     ];
